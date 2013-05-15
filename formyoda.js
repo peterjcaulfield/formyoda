a6d4bddb1b6f91c10ad24ae1da064d06932d9ccb
peterjcaulfield <peterjcaulfield@gmail.com>
2013-05-15 04:37:36 +0100

    added more comments

--- formyoda.js
+++ formyoda.js
@@ -5,17 +5,26 @@ if(document.readyState === "complete") {
 }
 // constuctor
 function Formyoda(){
+  /*
+  * Initialisation 
+   */
+
   // get reference to this for when we are nested and need ref to parent
   var that = this;
-  
+  // defaults to labels behind inputs
   this.labels = {inline : false};
+  // validation initialization
   this.validation = {};
+  // error messages for each validation function
   this.validation.errors = {blank : 'Please fill out this field', email : 'Invalid Email', maxchars : 'Too many characters'};
 
+  /*
+  * Validation functions
+  */
   this.validation.blank = function(input){
     if($(input).val() == ''){
-        $(input + '_yodalay').html(that.validation.errors.blank);
-        $(input + '_yodalay').addClass('error');
+        $(input + '_yodalabel').html(that.validation.errors.blank);
+        $(input + '_yodalabel').addClass('error');
         return false;
     }
     return true;
@@ -26,8 +35,8 @@ function Formyoda(){
     var regex = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
     if(!regex.test(email)){
       $(input).val('');
-      $(input + '_yodalay').html(that.validation.errors.email);
-      $(input + '_yodalay').addClass('error');
+      $(input + '_yodalabel').html(that.validation.errors.email);
+      $(input + '_yodalabel').addClass('error');
       return  false;
     }
     return true;
@@ -36,14 +45,12 @@ function Formyoda(){
   this.validation.max = function(array){
     if($(array[0]).val().length > array[1]){
       $(array[0]).val('');
-      $(array[0] + '_yodalay').html(that.validation.errors.maxchars);
-      $(array[0] + '_yodalay').addClass('error');
+      $(array[0] + '_yodalabel').html(that.validation.errors.maxchars);
+      $(array[0] + '_yodalabel').addClass('error');
       return false;
     }
     return true;
   }
-
-
 
   this.validate = function(inputs){
     errors = false;
@@ -81,14 +88,18 @@ function Formyoda(){
       return false;
     else
       return true;
-  }
+  } 
+
+  /*
+  * Function for adding labels to selected inputs with default placeholder  
+  */
 
   this.add_yodalabels = function(inputs){
     this.yodalabels = inputs;
     for(var propName in inputs){
     
       var id = '#' + propName;
-      var yodaid = propName + '_yodalay';
+      var yodaid = propName + '_yodalabel';
     
       if(this.labels.inline == false){
          // if not inline, labels are displayed behind the form inputs like placeholders
@@ -102,6 +113,7 @@ function Formyoda(){
         $(id).css({'z-index' : 10, 'position' : 'relative',  'background' : 'none'});
         $(id).parent().append('<div id="' + yodaid + '"></div>');
         $('#' + yodaid).css({'position' : 'absolute', 'top' : topPos, 'left' : leftPos, 'z-index': 0});
+        $('#' + yodaid).addClass('yodalabel');
         // set initial input value
         $('#' + yodaid).html(inputs[propName]);
   
@@ -109,9 +121,9 @@ function Formyoda(){
         $(id).focus(function(){
             var elem_id = $(this).attr('id');
             if($(this).val() == '')      
-              $('#' +  elem_id + '_yodalay').html('');
-            if($('#' +  elem_id + '_yodalay').hasClass('error')){
-                $('#' +  elem_id + '_yodalay').removeClass('error');
+              $('#' +  elem_id + '_yodalabel').html('');
+            if($('#' +  elem_id + '_yodalabel').hasClass('error')){
+                $('#' +  elem_id + '_yodalabel').removeClass('error');
                }
            });
 
@@ -119,10 +131,9 @@ function Formyoda(){
           
            var  elem_id = $(this).attr('id');
             if($(this).val() == '')
-              $('#' +  elem_id + '_yodalay').html(that.yodalabels[elem_id]);
+              $('#' +  elem_id + '_yodalabel').html(that.yodalabels[elem_id]);
           });
-      
-   
+
       }else
       { // we are dealing with labels to be displayed inline with the inputs 
         $(id).parent().css({'position': 'relative'});
@@ -132,12 +143,13 @@ function Formyoda(){
         $(id).parent().append('<div id="' + yodaid + '"></div>');
         $('#' + yodaid).html(inputs[propName]);
         $('#' + yodaid).css({'position' : 'absolute', 'top' : topPos, 'left' : leftPos, 'border' : 'none'});
+        $('#' + yodaid).addClass('yodalabel');
 
         $(id).focus(function(){
             var  elem_id = $(this).attr('id');
-            if($('#' + elem_id + '_yodalay').hasClass('error'))
-            $('#' + elem_id + '_yodalay').removeClass('error');
-            $('#' +  elem_id + '_yodalay').html(that.yodalabels[elem_id]);
+            if($('#' + elem_id + '_yodalabel').hasClass('error'))
+            $('#' + elem_id + '_yodalabel').removeClass('error');
+            $('#' +  elem_id + '_yodalabel').html(that.yodalabels[elem_id]);
           });
     
         $(id).blur(function(){
@@ -146,6 +158,7 @@ function Formyoda(){
     } // end of loop
   } // end of add function
 }// end of constructor
+
 // usage
 $(document).ready(function(){
 
